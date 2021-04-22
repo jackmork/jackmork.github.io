b0d243dab02e5573113fe360cde04de4a0431fe5
jackmork <Jackmork117@gmail.com>
2021-04-22 13:14:12 -0400

    new sketch

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,25 +1,24 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 400);
 
+  angleMode(DEGREES);
 }
 
 function draw() {
-  background(175);
-  strokeWeight(3);
-  for (i=0;i<801;i++) {
-    stroke(255-140/(i+1), 100+50*sin(.001*i*frameCount), 35+.1*i);
-    translate(30*cos(.001*i*frameCount),0);
-    point(200,.5*i);
-    point(155,.5*i);
-    point(110,.5*i);
-    point(65,.5*i);
-    point(20,.5*i);
-    point(245,.5*i);
-    point(290,.5*i);
-    point(335,.5*i);
-    point(380,.5*i);
-    point(420,.5*i);
+  strokeWeight(4);
+
+  let phase1 = 200;
+  let amp1 = 100;
+  let phase2 = 201.5;
+  let amp2 = 85;
+  let k=10;
+  for (i=0;i<2*width;i++) {
+  let y=amp1*sin(phase1*frameCount-k*i);
+  let p=amp2*sin(phase2*frameCount-k*i);
+    push();
+      stroke(100+ 50*cos(3*frameCount+2*i), 25+10*cos(i), 75);
+      translate(0, y+p);
+      point(i/2, 200);
+    pop();
   }
-
 }
-
