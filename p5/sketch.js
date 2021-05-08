492c6047cd71b922b57e2b1f2c4c03cae3afbee4
jackmork <Jackmork117@gmail.com>
2021-05-08 02:17:01 -0400

    Update sketch.js

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,13 +1,18 @@
 function setup() {
   createCanvas(400, 400);
-  background(random(100, 220));
+
+}
+
+function draw() {
+  frameRate(1.3);
+  curcle();
+}
+
+function curcle() {
+    background(random(100, 220));
   for (i=0;i<400;i++) {
     let c=color(random(25, 255), random(0, 250), random(0, 200));
     fill(c);
   circle(random(-50+.2*i, 400), random(0, 100+1.5*i), random(20, 110));
   }
-}
-
-function draw() {
-
 }
