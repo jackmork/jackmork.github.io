1e1eab7163e0b99c536adfa5a3b2d1db1ec4be35
jackmork <Jackmork117@gmail.com>
2021-05-07 23:59:06 -0400

    Update sketch.js

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,31 +1,13 @@
 function setup() {
-  createCanvas(800, 400);
-
-  angleMode(DEGREES);
+  createCanvas(400, 400);
+  background(random(100, 220));
+  for (i=0;i<400;i++) {
+    let c=color(random(25, 255), random(0, 250), random(0, 200));
+    fill(c);
+  circle(random(-50+.2*i, 400), random(0, 100+1.5*i), random(20, 110));
+  }
 }
 
 function draw() {
-  strokeWeight(4);
 
-  let phase1 = 200;
-  let amp1 = 110;
-  let phase2 = 201.5;
-  let amp2 = 95;
-  let k=10;
-  for (i=0;i<2*width;i++) {
-  let y=amp1*sin(phase1*frameCount-k*i);
-  let p=amp2*sin(phase2*frameCount-k*i);
-    push();
-      stroke(100+ 50*cos(3*frameCount+2*i), 25+10*cos(i), 75);
-      translate(0, y+p);
-      point(i/2, 200);
-    pop();
-  }
-  stroke(10, 228, 228);
-  strokeWeight(5);
-  line(0,0, 0, 800);
-  line(0,0, 800, 0);
-  stroke(50, 178, 228);
-  line(800,400, 0, 400);
-  line(800, 400, 800, 0);
 }
