1735fb1e3049e9ffb4997ab2884bc3baea3f4a5c
jackmork <Jackmork117@gmail.com>
2021-06-29 17:18:42 -0400

    Update sketch.js

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,50 +1,23 @@
-let num = 5000;
-let range = 15;
-let colorRange = 15;
-
-let ax = [];
-let ay = [];
-let ac = [];
-let i;
-
 function setup() {
   createCanvas(400, 400);
-  background(220);
-  strokeWeight(20);
-  angleMode(DEGREES);
-  for(i = 0; i < num; i++) {
-    ax[i] = width/2;
-    ay[i] = height/2;
-    ac[i] = 150;
-  }
-  
+
 }
 
 function draw() {
- alive();
+  frameRate(1.3);
+  curcle();
+  noLoop();
 }
 
-function alive() {
-   for( i = 1; i < num; i++) {
-    ax[i-1] = ax[i];
-    ay[i-1] = ay[i];
-    ac[i-1] = ac[i];
+function curcle() {
+    background(random(100, 220));
+  for (i=0;i<400;i++) {
+    let c=color(random(140, 285), random(100, 145), random(50, 1500));
+    fill(c);
+  circle(random(-50+.2*i, 400), random(0, 100+1.5*i), random(20, 110));
   }
+}
 
-  // Put a new value at the end of the array
-  ax[num-1] += random(-range, range);
-  ay[num-1] += random(-range, range);
-  ac[num-1] += random(-colorRange, colorRange);
-
-  // Constrain all points to the screen
-  ax[num-1] = constrain(ax[num-1], 0, width);
-  ay[num-1] = constrain(ay[num-1], 0, height);
-  ac[num-1] = constrain(ac[num-1], 25, 225);
-  
-   for( i=1; i<num; i++) {    
-    let val = color(20+ac[i-20], ac[i]-20, 40);
-    stroke(val);
-    point(ax[i], ay[i]);
-  }
-  
+function mousePressed() {
+  redraw();
 }
