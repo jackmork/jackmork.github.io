8da0773ca7c0ad237978628e68a5d07ff260b114
jackmork <78319277+jackmork@users.noreply.github.com>
2024-12-25 20:30:42 -0500

    homepage content

--- pages/home.py
+++ pages/home.py
@@ -1,7 +1,28 @@
-from dash import html
+from dash import callback_context, Dash, html, dcc, Input, Output
+import dash_bootstrap_components as dbc
 
-home_page = html.Div(
+home_page = dbc.Container(
     children=[
-    html.P("HOME IS NOT REAL")
+        dbc.Container(
+            children=[
+                html.P("Audiovisuals From My First Live Performance"),
+                html.Iframe(
+                    width="492",
+                    height="378",
+                    src="https://www.youtube.com/embed/lX30zbZIAGU?si=dYJeA-kgScLAXWEV"
+                )
+            ],
+            className='home-content'
+        ),
+        dbc.Container(
+            children=[
+                html.P("A Short, Esoteric Slasher Film"),
+                html.Iframe(
+                    width="492",
+                    height="378",
+                    src="https://www.youtube.com/embed/5GZmiO-6Xx8"
+                )
+            ],
+            className='home-content')
     ]
 )
