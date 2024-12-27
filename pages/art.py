da8e5e8119803c79685bf232f230e20ca4f5136d
jackmork <78319277+jackmork@users.noreply.github.com>
2024-12-26 20:43:29 -0500

    added art content

--- pages/art.py
+++ pages/art.py
@@ -1,7 +1,33 @@
-from dash import html
+from dash import html, dcc
+import dash_bootstrap_components as dbc
+
 
 art_page = html.Div(
     children=[
-    html.P("ART IS NOT REAL")
-    ]
+        dbc.Container(
+            children=[
+                html.Img(
+                    src='../assets/instagram_picture.png',
+                    style={'width': '200px', 'height': '200px'}),
+                html.A(
+                    children="Visionary Module Instagram Page",
+                    href='https://www.instagram.com/visionary_module/',
+                    target='_blank',
+                    style={'white-space': 'nowrap'})],
+            className='art-content'
+            ),
+        dbc.Container(
+            children=[
+                html.Img(
+                    src='../assets/youtube_picture.png',
+                    style={'width': '250px', 'height': '200px'}),
+                html.A(
+                    children="Visionary Module Youtube Channel",
+                    href='https://www.youtube.com/@VisionaryModule',
+                    target='_blank',
+                    style={'white-space': 'nowrap'})
+                    ],
+            className='art-content'
+            )
+    ],
 )
